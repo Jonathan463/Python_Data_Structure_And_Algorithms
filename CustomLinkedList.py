c4d0707b95a28cbc30339949ca94a40fb5dbd496
jg24aat@herts.ac.uk <jg24aat@herts.ac.uk>
2025-06-14 23:40:21 +0100

    Implemented LinkedList

---
+++ CustomLinkedList.py
@@ -0,0 +1,57 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None  # Tail pointer
+
+    def is_empty(self):
+        return self.head is None
+
+    def append(self, data):
+       new_record = Node(data)
+       if self.is_empty():
+           self.head = self.tail = new_record
+       else:
+            self.tail.next = new_record
+            self.tail = new_record
+
+
+    def prepend(self, data):
+        new_record = Node(data)
+        if self.is_empty():
+            self.head = self.tail = new_record
+        else:
+            new_record.next = self.head
+            self.head = new_record
+
+    def to_list(self):
+        elements = []
+        current = self.head
+        while current:
+            elements.append(current.data)
+            current = current.next
+        return elements
+
+    def display(self):
+        print("LinkedList:", self.to_list())
+
+    def length(self):
+        count = 0
+        current = self.head
+        while current:
+            count += 1
+            current = current.next
+        return count
+
+
+if __name__ == "__main__":
+    linkedList = LinkedList()
+    linkedList.append(2)
+    linkedList.append(4)
+    linkedList.append(6)
+    linkedList.display()
